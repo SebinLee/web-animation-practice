d860129675a64c6a0ef575a1281bffebd4177a46
SebinLee <sebinlee.artdev@gmail.com>
2021-07-18 22:13:49 +0900

    Add iPadIntroduction Screen

---
+++ src/component/IPadIntroduction.tsx
@@ -0,0 +1,18 @@
+import "./IPadIntroduction.css";
+import iPadImage from "./image/iPadPro.jpeg";
+
+export default function IPadIntroduction() {
+	return (
+		<div className="container">
+			<img src={iPadImage} className="image" />
+			<div className="introductionContainer">
+				<h1 className="heading">iPad Pro</h1>
+				<h2 className="text">
+					막강한 성능의
+					<br />
+					Apple M1 칩 탑재.
+				</h2>
+			</div>
+		</div>
+	);
+}
