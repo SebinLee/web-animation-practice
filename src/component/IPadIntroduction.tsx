086116394b0c0f53297e88ad11f5e9f7ecb08f44
SebinLee <sebinlee.artdev@gmail.com>
2021-07-19 23:02:04 +0900

    Add Basic zoom in-out animation

--- src/component/IPadIntroduction.tsx
+++ src/component/IPadIntroduction.tsx
@@ -1,11 +1,32 @@
 import "./staticStyle.css";
+import "./animatedStyle.css";
 import iPadImage from "./image/iPadPro.jpeg";
 
 export default function IPadIntroduction() {
+	return <IPadIntroductionAnimated />;
+}
+
+function IPadIntroductionStatic() {
 	return (
 		<div className="container">
 			<img src={iPadImage} className="image" />
 			<div className="overlay">
+				<h1 className="heading">iPad Pro</h1>
+				<h2 className="text">
+					막강한 성능의
+					<br />
+					Apple M1 칩 탑재.
+				</h2>
+			</div>
+		</div>
+	);
+}
+
+function IPadIntroductionAnimated() {
+	return (
+		<div className="container">
+			<img src={iPadImage} className="image zoomOut" />
+			<div className="overlay zoomIn">
 				<h1 className="heading">iPad Pro</h1>
 				<h2 className="text">
 					막강한 성능의
